c43f28aa9fb92fbfc67b5905020064fbd4891ec1
Tinyfish <tifish@gmail.com>
2017-04-17 23:57:58 +0800

    Release 4.5

--- 黑域补丁制作byTinyfish.bat
+++ 黑域补丁制作byTinyfish.bat
@@ -5,7 +5,7 @@ color 3f
 cd /d "%~dp0"
 set "path=%~dp0Binary;%~dp0jre\bin;!path!"
 
-title 黑域补丁自动制作 4.4 by Tinyfish
+title 黑域补丁自动制作 4.5 by Tinyfish
 echo =================================================
 echo   此脚本一键制作黑域内核补丁，功能包括：
 echo   * 自动上传下载手机中的内核文件。
